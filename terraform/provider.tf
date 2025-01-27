aa8060b62c2896d3fbf38b1c24cb9baf9348e480
svenkatesh293 <santoshvenkatesh293@gmail.com>
2025-01-27 12:38:51 +0000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,16 @@
 locals {
-  region = "us-east-1"
-  name   = "kastro-eks-cluster"
-  vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
-  }
+    region = "eu-west-1"
+    name   = "Santosh-eks-cluster"
+    vpc_cidr = "10.123.0.0/16"
+    azs      = ["eu-west-1a", "euwest-1b"]
+    public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+    private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+    intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+    tags = {
+        Example = local.name
+    }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
